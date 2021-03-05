3d042052880ed1244370ddaa144741c3515250b8
frosychr <el17178@mail.ntua.gr>
2021-03-05 04:28:19 +0200

    Initial commit from frosy

--- app/src/main/java/com/example/due/SetProfile.kt
+++ app/src/main/java/com/example/due/SetProfile.kt
@@ -3,8 +3,12 @@ package com.example.due
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
+import android.widget.AdapterView
 import android.widget.Button
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 
 class SetProfile : AppCompatActivity() {
@@ -17,6 +21,28 @@ class SetProfile : AppCompatActivity() {
         button1.setOnClickListener {
             val intent1 = Intent(this,ChooseInterests::class.java)
             startActivity(intent1)
+        }
+
+
+        val geenders = arrayOf("Gender*","Male", "Female")
+        val arrayAdapter = ArrayAdapter(this, R.layout.textspinner, geenders)
+        val spg = findViewById<Spinner>(R.id.spinner_gender)
+        spg.adapter = arrayAdapter
+        spg.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                //To change body of created
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //To change body of created
+            }
+
         }
     }
 }
