0008f36d0576858572c5bfd3c0be8521121a5616
Komni <jimkomninos@hotmail.com>
2021-03-06 21:11:41 +0200

    Add fuctionality to all buttons.

--- app/src/main/java/com/example/due/HomePageNew.kt
+++ app/src/main/java/com/example/due/HomePageNew.kt
@@ -9,9 +9,11 @@ import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FileDownloadTask
@@ -30,9 +32,13 @@ class HomePageNew : AppCompatActivity() {
 
         val db = Firebase.firestore
         val storageRef = FirebaseStorage.getInstance().reference;
-        //val storageRef: StorageReference = FirebaseStorage.getReference()
 
-        db.collection("test_users")
+        val currentUsername = "Jack"
+        val currentUserId = "ZQZEsGUBpxxFvU026MeW"
+        val seenUsers: ArrayList<UserInfo> = arrayListOf()
+
+
+        db.collection("test_users").whereNotEqualTo("username", currentUsername)
             .get()
             .addOnSuccessListener { result ->
 
@@ -43,20 +49,26 @@ class HomePageNew : AppCompatActivity() {
                 for (document in result) {
                     Log.d("test_users", "${document.id} => ${document.data}")
                     val userInfo = document.toObject(UserInfo::class.java) ?: UserInfo()
+                    userInfo.id = document.id
                     users.add(userInfo)
                 }
 
+
+                val notSeenUsers = users.filter { s -> s !in seenUsers }
+                var displayedUser = notSeenUsers.shuffled().take(1)[0]
+
                 // Display the first user
-                userAgeTextView.text = "${users[0].username}, ${users[0].age}"
-                val riversRef: StorageReference = storageRef.child("${users[0].username}.jpg")
-                val localFile = File.createTempFile("images", "jpg")
-                riversRef.getFile(localFile)
+                seenUsers.add(displayedUser)
+                userAgeTextView.text = "${displayedUser.username}, ${displayedUser.age}"
+                val startUserImagesRef: StorageReference = storageRef.child("${displayedUser.username}.jpg")
+                val startLocalFile = File.createTempFile("images", "jpg")
+                startUserImagesRef.getFile(startLocalFile)
                         .addOnSuccessListener(OnSuccessListener<FileDownloadTask.TaskSnapshot?> {
                             Log.d("photo", "success")
 
                             // Display user image
                             val userImage: ImageView = findViewById<View>(R.id.user_image) as ImageView
-                            val myBitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+                            val myBitmap = BitmapFactory.decodeFile(startLocalFile.absolutePath)
                             userImage.setImageBitmap(myBitmap)
 
                             Log.d("photo", "display")
@@ -65,22 +77,41 @@ class HomePageNew : AppCompatActivity() {
                             Log.d("photo", "fail")
                         })
 
+
                 // Like button pressed
                 val likeButton: Button = findViewById(R.id.like_button)
                 likeButton.setOnClickListener {
 
-                    // Display users
-                    if(users.size > 1) {
-                        val userRange = 1 until users.size
-                        val userIndex = userRange.random()
-                        Log.d("index", userIndex.toString())
-                        userAgeTextView.text = "${users[userIndex].username}, ${users[userIndex].age}"
+                    // Check if it's a match
+                    if(currentUsername in displayedUser.my_likes) {
+                        Toast.makeText(applicationContext, "IT'S A MATCH!!!", Toast.LENGTH_SHORT).show()
+                    }
+
+                    // Update the database about the like reaction
+                    displayedUser.got_likes.add(currentUsername)
+                    val data = hashMapOf("got_likes" to displayedUser.got_likes)
+                    db.collection("test_users").document(displayedUser.id)
+                            .set(data, SetOptions.merge())
+                            .addOnSuccessListener {}
+                            .addOnFailureListener {
+                                Toast.makeText(applicationContext,"egine malakia" ,Toast.LENGTH_SHORT).show()
+                            }
+
+                    val notSeenUsers = users.filter { s -> s !in seenUsers }
+
+                    // Display next user
+                    if(notSeenUsers.isNotEmpty()) {
 
 
-                        // Download user photo
-                        val riversRef: StorageReference = storageRef.child("${users[userIndex].username}.jpg")
+
+                        // Get the new one
+                        displayedUser = notSeenUsers.shuffled().take(1)[0]
+                        seenUsers.add(displayedUser)
+
+                        // Download User Image
+                        val userImagesRef: StorageReference = storageRef.child("${displayedUser.username}.jpg")
                         val localFile = File.createTempFile("images", "jpg")
-                        riversRef.getFile(localFile)
+                        userImagesRef.getFile(localFile)
                                 .addOnSuccessListener(OnSuccessListener<FileDownloadTask.TaskSnapshot?> {
                                     Log.d("photo", "success")
 
@@ -89,36 +120,48 @@ class HomePageNew : AppCompatActivity() {
                                     val myBitmap = BitmapFactory.decodeFile(localFile.absolutePath)
                                     userImage.setImageBitmap(myBitmap)
 
-                                    Log.d("photo", "display")
+                                    // Display Username
+                                    userAgeTextView.text = "${displayedUser.username}, ${displayedUser.age}"
 
                                 }).addOnFailureListener(OnFailureListener {
                                     Log.d("photo", "fail")
                                 })
 
-
-                        users.removeAt(userIndex)
                     }
                     else{
                         userAgeTextView.text = "den exei alla mounakia bro"
                     }
+
                 }
 
                 // Dislike button pressed
                 val dislikeButton: Button = findViewById(R.id.dislike_button)
                 dislikeButton.setOnClickListener {
 
-                    // Display users
-                    if(users.size > 1) {
-                        val userRange = 1 until users.size
-                        val userIndex = userRange.random()
-                        Log.d("index", userIndex.toString())
-                        userAgeTextView.text = "${users[userIndex].username}, ${users[userIndex].age}"
+                    // Update the database about the like reaction
+                    displayedUser.got_dislikes.add(currentUsername)
+                    val data = hashMapOf("got_dislikes" to displayedUser.got_dislikes)
+                    db.collection("test_users").document(displayedUser.id)
+                            .set(data, SetOptions.merge())
+                            .addOnSuccessListener {}
+                            .addOnFailureListener {
+                                Toast.makeText(applicationContext,"egine malakia" ,Toast.LENGTH_SHORT).show()
+                            }
+
+                    val notSeenUsers = users.filter { s -> s !in seenUsers }
+
+                    // Display next user
+                    if(notSeenUsers.isNotEmpty()) {
+
+                        // Get the new one
+                        displayedUser = notSeenUsers.shuffled().take(1)[0]
+                        seenUsers.add(displayedUser)
 
 
-                        // Download user photo
-                        val riversRef: StorageReference = storageRef.child("${users[userIndex].username}.jpg")
+                        // Download User Image
+                        val userImagesRef: StorageReference = storageRef.child("${displayedUser.username}.jpg")
                         val localFile = File.createTempFile("images", "jpg")
-                        riversRef.getFile(localFile)
+                        userImagesRef.getFile(localFile)
                                 .addOnSuccessListener(OnSuccessListener<FileDownloadTask.TaskSnapshot?> {
                                     Log.d("photo", "success")
 
@@ -127,36 +170,52 @@ class HomePageNew : AppCompatActivity() {
                                     val myBitmap = BitmapFactory.decodeFile(localFile.absolutePath)
                                     userImage.setImageBitmap(myBitmap)
 
-                                    Log.d("photo", "display")
+                                    // Display Username
+                                    userAgeTextView.text = "${displayedUser.username}, ${displayedUser.age}"
 
                                 }).addOnFailureListener(OnFailureListener {
                                     Log.d("photo", "fail")
                                 })
 
-
-                        users.removeAt(userIndex)
                     }
                     else{
                         userAgeTextView.text = "den exei alla mounakia bro"
                     }
+
                 }
 
                 // Drink button pressed
                 val drinkButton: Button = findViewById(R.id.drink_button)
                 drinkButton.setOnClickListener {
 
-                    // Display users
-                    if(users.size > 1) {
-                        val userRange = 1 until users.size
-                        val userIndex = userRange.random()
-                        Log.d("index", userIndex.toString())
-                        userAgeTextView.text = "${users[userIndex].username}, ${users[userIndex].age}"
+                    // Check if it's a match
+                    if(currentUsername in displayedUser.my_likes) {
+                        Toast.makeText(applicationContext, "IT'S A MATCH!!!", Toast.LENGTH_SHORT).show()
+                    }
 
+                    // Update the database about the like reaction
+                    displayedUser.got_drinks.add(currentUsername)
+                    val data = hashMapOf("got_drinks" to displayedUser.got_drinks)
+                    db.collection("test_users").document(displayedUser.id)
+                            .set(data, SetOptions.merge())
+                            .addOnSuccessListener {}
+                            .addOnFailureListener {
+                                Toast.makeText(applicationContext,"egine malakia" ,Toast.LENGTH_SHORT).show()
+                            }
 
-                        // Download user photo
-                        val riversRef: StorageReference = storageRef.child("${users[userIndex].username}.jpg")
+                    val notSeenUsers = users.filter { s -> s !in seenUsers }
+
+                    // Display next user
+                    if(notSeenUsers.isNotEmpty()) {
+
+                        // Get the new one
+                        displayedUser = notSeenUsers.shuffled().take(1)[0]
+                        seenUsers.add(displayedUser)
+
+                        // Download User Image
+                        val userImagesRef: StorageReference = storageRef.child("${displayedUser.username}.jpg")
                         val localFile = File.createTempFile("images", "jpg")
-                        riversRef.getFile(localFile)
+                        userImagesRef.getFile(localFile)
                                 .addOnSuccessListener(OnSuccessListener<FileDownloadTask.TaskSnapshot?> {
                                     Log.d("photo", "success")
 
@@ -165,18 +224,18 @@ class HomePageNew : AppCompatActivity() {
                                     val myBitmap = BitmapFactory.decodeFile(localFile.absolutePath)
                                     userImage.setImageBitmap(myBitmap)
 
-                                    Log.d("photo", "display")
+                                    // Display Username
+                                    userAgeTextView.text = "${displayedUser.username}, ${displayedUser.age}"
 
                                 }).addOnFailureListener(OnFailureListener {
                                     Log.d("photo", "fail")
                                 })
 
-
-                        users.removeAt(userIndex)
                     }
                     else{
                         userAgeTextView.text = "den exei alla mounakia bro"
                     }
+
                 }
 
             }
@@ -187,6 +246,7 @@ class HomePageNew : AppCompatActivity() {
 }
 
 data class UserInfo(
+        var id: String = "",
         var username: String = "",
         var age: Int = 0,
         var my_likes: ArrayList<String> = arrayListOf(),
